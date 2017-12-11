7b7ae5c4f71a228a60e6db5ce96c701eacbad0de
figlero <Lutz.Liebig@stud.uni-due.de>
2017-12-11 14:50:49 +0100

    validate entitiy angefangen

---
+++ js/validate/validateEntity.js
@@ -0,0 +1,29 @@
+function checkName(){
+    var word = $('#entityName').val();
+
+    alert(jsonp('https://api.datamuse.com/words?sp=' + word +
+        '&md=d&max=1'));
+
+}
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    var json = JSON.parse(xmlHttp.responseText);
+    return json["defHeadword"];
+}
+
+function jsonp(url){
+
+
+    $.getJSON(url, function(response) {
+        $.each(response, function(key, val) {
+            var obj = response[0];
+            var name = obj[0];
+            console.log(obj);
+            return obj;
+        });
+    });
+}
