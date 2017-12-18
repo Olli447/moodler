519ff53a97783e3584d77c6e6b937748937177c9
figlero <Lutz.Liebig@stud.uni-due.de>
2017-12-18 17:50:00 +0100

    alert kardinalittäscheck

--- js/validate/validateCardinality.js
+++ js/validate/validateCardinality.js
@@ -1,10 +1,13 @@
-function checkCardi(lower,higher){
-   var b = true;
-    if(lower==0 && higher ==0){
-        b= false;
-    }
-    else if(lower>higher){
+function checkCardi(element) {
+    var b = true;
+    if (element.val().charAt(2)!= '*') {
+    var lower = parseInt(element.val().charAt(0));
+    var higher = parseInt(element.val().charAt(2));
+    if(lower>higher){
+        element.val("");
         b=false;
+        $('#clickModalCardinal').click();
     }
+}
     return b;
 }
