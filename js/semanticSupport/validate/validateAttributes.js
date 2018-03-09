dfb2aa4c4d726990873960a4563a315308b020d8
lutzpro <lutzschulmail@web.de>
2018-03-09 14:15:15 +0100

    atomar etc.

--- js/semanticSupport/validate/validateAttributes.js
+++ js/semanticSupport/validate/validateAttributes.js
@@ -11,7 +11,7 @@ function checkAtomar() {
 
     for (var i = 0; i < attributes.length; i++) {
         console.log("dada" + attributes[i]);
-        switch(attributes[i].propertyName){
+        switch (attributes[i].propertyName) {
             case "Adresse":
                 var modal = notification.createWarning("Hinweis", "<div class='row' style='margin-left: 0.33333%;'><p>Adresse sollte nicht als Attribut modelliert werden. Wollen Sie die Entitätstypen Adresse, Stadt und Land hinzufügen?</p></div><div class='row col-md-offset-1' style=\"margin-left: 0.33333%;\"><div></div><button type='button' class='btn btn-raised btn-success btn-block' style='width: 45%; margin: 2.5%' id='yesAdresse'>Ja</button><button type='button' class='btn btn-raised btn-danger btn-block' style='width: 45%; margin: 2.5%' id='noAdresse'>Nein</button></div>");
                 modal.find("#yesAdresse").on("click", function () {
@@ -64,14 +64,13 @@ function checkAtomar() {
 
                     //remove Atribute by deleting and creating new Entity
                     var data = moodler.getEntityData(key);
-                    console.log(data);
-                    for(var i = 0; i< data.properties.length; i++){
-                        if(data.properties[i].propertyName=="Adresse"){
-                            data.properties.splice(i,1);
+                    for (var i = 0; i < data.properties.length; i++) {
+                        if (data.properties[i].propertyName == "Adresse") {
+                            data.properties.splice(i, 1);
                         }
                     }
                     var entitys = moodler.getEntityList();
-                    for(var i = 0; i<entitys.length;i++) {
+                    for (var i = 0; i < entitys.length; i++) {
                         console.log(entitys[i]);
                         if (entitys[i].key == key) {
                             moodler.deleteEntity(key);
@@ -99,7 +98,7 @@ function checkAtomar() {
                         sourceRole: "",
                         sourceMultiplicity: "1,*",
                         targetRole: "",
-                        targetMultiplicity: "1,*"
+                        targetMultiplicity: "1,1"
                     };
                     moodler.addRelationship(linkOrt, 0, 0);
 
@@ -107,7 +106,7 @@ function checkAtomar() {
                     var linkLand = {
                         source: "Stadt",
                         target: "Land",
-                        name: "liegt_in",
+                        name: "ist_in",
                         sourceRole: "",
                         sourceMultiplicity: "1,*",
                         targetRole: "",
@@ -119,17 +118,71 @@ function checkAtomar() {
                 });
                 break;
             case "Name":
-                var modal = notification.createWarning("Hinweis", "<div class='row' style='margin-left: 0.33333%;'><p>Ein Name sollte nicht alleine stehen und in Name und Vorname aufgeteilt werden. Möchtest du Vorname hinzufügen?</p></div><div class='row col-md-offset-1' style=\"margin-left: 0.33333%;\"><div></div><button type='button' class='btn btn-raised btn-success btn-block' style='width: 45%; margin: 2.5%' id='yesName'>Ja</button><button type='button' class='btn btn-raised btn-danger btn-block' style='width: 45%; margin: 2.5%' id='noName'>Nein</button></div>");
-                modal.find("#yesName").on("click", function () {
-
-                });
-                modal.find("#noName").on("click", function () {
-                });
+                if (key == "Person" && existVorname() == false) {
+                    var modal = notification.createWarning("Hinweis", "<div class='row' style='margin-left: 0.33333%;'><p>Ein Name sollte nicht alleine stehen und in Name und Vorname aufgeteilt werden. Möchtest du Vorname hinzufügen?</p></div><div class='row col-md-offset-1' style=\"margin-left: 0.33333%;\"><div></div><button type='button' class='btn btn-raised btn-success btn-block' style='width: 45%; margin: 2.5%' id='yesName'>Ja</button><button type='button' class='btn btn-raised btn-danger btn-block' style='width: 45%; margin: 2.5%' id='noName'>Nein</button></div>");
+                    modal.find("#yesName").on("click", function () {
+                        var data = moodler.getEntityData("Person");
+                        data.properties.push({
+                            propertyName: "Vorname",
+                            propertyType: "String"
+                        });
+                        var entitys = moodler.getEntityList();
+                        for (var i = 0; i < entitys.length; i++) {
+                            if (entitys[i].key == "Person") {
+                                moodler.deleteEntity("Person");
+                            }
+                        }
+                        moodler.addEntity(data, 0, 0);
+                    });
+                    modal.find("#noName").on("click", function () {
+                    });
+                }
                 break;
             case "Alter":
+                var modal = notification.createWarning("Hinweis", "<div class='row' style='margin-left: 0.33333%;'><p>Das Alter sollte lieber als Geburtsdatum modelliert werden. Soll es automatisch ersetzt werden?</p></div><div class='row col-md-offset-1' style=\"margin-left: 0.33333%;\"><div></div><button type='button' class='btn btn-raised btn-success btn-block' style='width: 45%; margin: 2.5%' id='yesAlter'>Ja</button><button type='button' class='btn btn-raised btn-danger btn-block' style='width: 45%; margin: 2.5%' id='noAlter'>Nein</button></div>");
+                modal.find("#yesAlter").on("click", function () {
+                    var data = moodler.getEntityData(key);
+                    for (var i = 0; i < data.properties.length; i++) {
+                        if (data.properties[i].propertyName == "Alter") {
+                            data.properties.splice(i, 1);
+                        }
+                    }
+                    data.properties.push({
+                        propertyName: "Geburtsdatum",
+                        propertyType: "String"
+                    });
+                    var entitys = moodler.getEntityList();
+                    for (var i = 0; i < entitys.length; i++) {
+                        if (entitys[i].key == key) {
+                            moodler.deleteEntity(key);
+                        }
+                    }
+                    moodler.addEntity(data, 0, 0);
+                    });
+                modal.find("#noAlter").on("click", function () {
+                });
                 break;
             case "Jahresgehalt":
                 break;
         }
     }
+}
+
+function existVorname() {
+    var modal = $("#entity-modal");
+    var attributes = [];
+    var key = $('#entityName').val();
+
+    modal.find(".cloneable").each(function () {
+        attributes.push({
+            propertyName: $(this).find("#attributeName").val()
+        });
+    });
+
+    for (var i = 0; i < attributes.length; i++) {
+        if (attributes[i].propertyName == "Vorname") {
+            return true;
+        }
+    }
+    return false;
 }
